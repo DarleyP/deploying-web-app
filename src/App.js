4c020902e071df33bce480c1ce840c06a51ac14d
Darley Peralta <ordeal00s@gmail.com>
2023-07-31 17:08:38 -0400

    dp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react'
+import Form from './Form';
+import TodoList from './TodoList';
+
+
+
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+
+  handleToggle = (clickId) => {
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+          return todo
+        
+      })
+    })
+  }
+
+  handleAdd = (task) => {
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+
+
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className='mainDiv'>
+        <h1>To Do List </h1>
+        <TodoList handleToggle = {this.handleToggle} todos={todos} />
+        <Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/Form.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+  handleAdd = (e) => {
+    e.preventDefault();
+    
+    this.props.handleAdd(this.state.input);
+
+
+   
+    
+  }
+
+  handleChange= (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+      
+
+    })
+  }
+
+  render() {
+
+    return (
+      <div>
+      <form>
+          <input onChange={this.handleChange}/>
+          <button onClick={this.handleAdd}>Add</button>
+        </form>
+      </div>
+    )
+  }
+}
